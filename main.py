063d27a5553af3624dc9b9ee7921a2d7c254a753
Maxim Kopnev <kopnevmaxim@mail.ru>
2021-11-27 19:07:58 +0300

    Commit 3

--- main.py
+++ main.py
@@ -1,38 +1,27 @@
 import sys
-from PyQt5 import uic
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 
 
 class Form(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
-        self.screen = ''
-        self.data = ''
-        for i in self.buttonGroup_digits.buttons():
-            i.clicked.connect(self.run)
-        self.btn_clear.clicked.connect(self.clear)
-        for i in self.buttonGroup_binary.buttons():
-            i.clicked.connect(self.operation)
-        self.btn_eq.clicked.connect(self.result)
+        self.btn = QPushButton("->", self)
+        self.side = False
+        self.initUI()
 
-    def clear(self):
-        self.screen = ''
-        self.data = ''
-        self.table.display(0)
+    def initUI(self):
+        self.setGeometry(300, 300, 93 * 3, 28)
+        self.setWindowTitle('Circles')
+        self.btn.resize(self.btn.sizeHint())
+        self.btn.move(93, 0)
+        self.btn.clicked.connect(self.convert)
 
     def run(self):
-        if self.data and self.data[-1].isdigit():
-            self.screen += self.sender().text()
-        else:
-            self.screen = self.sender().text()
-        self.data += self.sender().text()
-        print(self.data)
-        self.table.display(int(self.screen))
+        pass
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    form = Form()
-    form.show()
+    ex = Form()
+    ex.show()
     sys.exit(app.exec())
